5bce35d43e25051606a6207a53e46603011e4609
Sofilus <sofia00olsson@gmail.com>
2023-03-01 20:40:08 +0100

    fetch to get list of books and show the list on the web page

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 let bookListContainer = document.querySelector('#bookList');
 
-function createBookList (){
+fetch('http://localhost:3000/books')
+.then(res => res.json())
+.then(data => {
+   createBookList(data)
+})
+
+function createBookList (books){
+   
+   books.map(book => {
+
    let bookTitle = document.createElement("h2");
+   console.log(bookTitle)
+   bookTitle.innerHTML = book.name;
    let borrowedOrNot = document.createElement("p");
+   borrowedOrNot.innerHTML = book.borrowed; 
    bookListContainer.appendChild(bookTitle);
-   bookListContainer.appendChild(borrowedOrNot)  
+   bookListContainer.appendChild(borrowedOrNot)
+   })
 }
-createBookList();
